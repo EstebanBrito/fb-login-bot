9c8d5a457752b7feaec61f133766ff27140d66b2
EstebanBrito <esteban.brito.borges@gmail.com>
2019-09-15 19:59:57 -0500

    Second commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+print('FB LOGIN BOT:')
+email_string = input('Write down your email:')
+pass_string = input('Write down your password:')
+# Open Chrome and load the FB login page
+driver = webdriver.Chrome("/usr/bin/chromedriver")
+driver.get('https://www.facebook.com/login.php?login_attempt=1&lwv=110')
+print("Opened facebook...")
+# Find the email form input
+email = driver.find_element_by_xpath("//input[@id='email' or @name='email']")
+email.send_keys('game12_2908@hotmail.com')
+print("Email entered...")
+# Find the password form input
+password = driver.find_element_by_xpath("//input[@id='pass']")
+password.send_keys(pass_string)
+print("Password entered...")
+# Click the log in button
+button = driver.find_element_by_xpath("//button[@id='loginbutton']")
+button.click()
+print("Logued in into FB")
+
